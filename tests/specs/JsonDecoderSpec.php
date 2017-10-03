b8f0a0057374a9e64b319ebfb2704de051d956f3
johannes.pichler <johannes.pichler@karriere.at>
2017-10-03 10:29:00 +0200

    apply fixes from style ci

--- tests/specs/JsonDecoderSpec.php
+++ tests/specs/JsonDecoderSpec.php
@@ -2,7 +2,6 @@
 
 namespace tests\specs\Karriere\JsonDecoder;
 
-use Karriere\JsonDecoder\Bindings\AliasBinding;
 use Karriere\JsonDecoder\Bindings\CallbackBinding;
 use Karriere\JsonDecoder\ClassBindings;
 use Karriere\JsonDecoder\JsonDecoder;
@@ -11,29 +10,29 @@ use PhpSpec\ObjectBehavior;
 
 class JsonDecoderSpec extends ObjectBehavior
 {
-    function let()
+    public function let()
     {
         $this->beConstructedWith(false, false);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(JsonDecoder::class);
     }
 
-    function it_should_allow_decoding_of_private_properties()
+    public function it_should_allow_decoding_of_private_properties()
     {
         $this->beConstructedWith(true, false);
         $this->decodesPrivateProperties()->shouldReturn(true);
     }
 
-    function it_should_allow_decoding_of_protected_properties()
+    public function it_should_allow_decoding_of_protected_properties()
     {
         $this->beConstructedWith(false, true);
         $this->decodesProtectedProperties()->shouldReturn(true);
     }
 
-    function it_should_transform_raw_data()
+    public function it_should_transform_raw_data()
     {
         $jsonString = '{"id": 1, "name": "John Doe"}';
 
@@ -41,10 +40,10 @@ class JsonDecoderSpec extends ObjectBehavior
 
         $response->shouldHaveType(JsonDecoderSample::class);
         $response->id->shouldBe(1);
-        $response->name->shouldBe("John Doe");
+        $response->name->shouldBe('John Doe');
     }
 
-    function it_should_transform_by_using_custom_transformer()
+    public function it_should_transform_by_using_custom_transformer()
     {
         $this->register(new SampleTransformer());
 
@@ -54,10 +53,10 @@ class JsonDecoderSpec extends ObjectBehavior
 
         $response->shouldHaveType(JsonDecoderSample::class);
         $response->id->shouldBe(1);
-        $response->name->shouldBe("John Doe");
+        $response->name->shouldBe('John Doe');
     }
 
-    function it_should_transform_empty_raw_data_to_null()
+    public function it_should_transform_empty_raw_data_to_null()
     {
         $jsonString = 'null';
 
@@ -65,12 +64,14 @@ class JsonDecoderSpec extends ObjectBehavior
     }
 }
 
-class JsonDecoderSample {
+class JsonDecoderSample
+{
     public $id;
     public $name;
 }
 
-class SampleTransformer implements Transformer {
+class SampleTransformer implements Transformer
+{
 
     public function register(ClassBindings $classBindings)
     {
