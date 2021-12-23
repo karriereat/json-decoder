74460a51b990d58d378c4368926bd887f3b85211
Jakob Linskeseder <jakob.linskeseder@karriere.at>
2021-12-23 13:08:59 +0100

    Avoid IDE warnings when passing Closure to CallbackBinding
    Fixes #43

--- src/Bindings/CallbackBinding.php
+++ src/Bindings/CallbackBinding.php
@@ -2,6 +2,7 @@
 
 namespace Karriere\JsonDecoder\Bindings;
 
+use Closure;
 use Karriere\JsonDecoder\Binding;
 use Karriere\JsonDecoder\JsonDecoder;
 use Karriere\JsonDecoder\Property;
@@ -9,14 +10,17 @@ use Karriere\JsonDecoder\Property;
 class CallbackBinding extends Binding
 {
     /**
-     * @var callable
+     * @var Closure
      */
     private $callback;
 
     /**
      * CallbackBinding constructor.
+     *
+     * @param string $property
+     * @param Closure $callback
      */
-    public function __construct(string $property, callable $callback)
+    public function __construct(string $property, Closure $callback)
     {
         parent::__construct($property, null, null, false);
         $this->callback = $callback;
